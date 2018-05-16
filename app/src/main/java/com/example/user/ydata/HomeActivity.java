d399e4fd5cbb43a14a1c475138d09b9cf86d58ba
thanosmak <makaronasthanos@gmail.com>
2018-05-17 01:19:54 +0300

    Third Push - Home View

---
+++ app/src/main/java/com/example/user/ydata/HomeActivity.java
@@ -0,0 +1,42 @@
+package com.example.user.ydata;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.view.View;
+
+public class HomeActivity extends AppCompatActivity implements View.OnClickListener{
+    private CardView watermeterCard, lastMovesCard, billsCard, profileCard;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        // Define Cards
+        watermeterCard = (CardView) findViewById(R.id.watermeterButton);
+        lastMovesCard = (CardView) findViewById(R.id.latestMovesButton);
+        billsCard = (CardView) findViewById(R.id.billsButton);
+        profileCard = (CardView) findViewById(R.id.profileButton);
+
+        // Set click listeners
+        watermeterCard.setOnClickListener(this);
+        lastMovesCard.setOnClickListener(this);
+        billsCard.setOnClickListener(this);
+        profileCard.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        Intent i;
+
+        switch (view.getId()){
+            case R.id.watermeterButton : i = new Intent(this, WatermeterActivity.class); startActivity(i); break;
+            case R.id.latestMovesButton : i = new Intent(this, LastMovesActivity.class); startActivity(i); break;
+            case R.id.billsButton : i = new Intent(this, BillsActivity.class); startActivity(i); break;
+            case R.id.profileButton : i = new Intent(this, ProfileActivity.class); startActivity(i); break;
+            default:break;
+        }
+    }
+}
